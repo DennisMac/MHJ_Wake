b2a0f4d12d5a3603d60a7a13c5e3ae3bfe0537bf
natmaxex <nathanrumsey@gmail.com>
2018-04-14 14:49:17 -0400

    Nathan

---
+++ MHJWake/Assets/Nathans Work/script/menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //PlayerPrefs.SetInt("with or without sound", 20);//with or without sound
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    //vocal guide lead, solo guide lead
+    public void VocalG() {
+        SceneManager.LoadScene("SoloGuideLeadLead", LoadSceneMode.Single);
+        Debug.Log("SoloGuideLeadLead Loading");
+    }
+
+    public void SoloG() {
+        SceneManager.LoadScene("VocalGuideLeadLevel", LoadSceneMode.Single);
+        Debug.Log("VocalGuideLeadLevel Loading");
+    }
+}
