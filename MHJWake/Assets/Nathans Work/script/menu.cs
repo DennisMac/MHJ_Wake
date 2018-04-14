c8bc25b6aabb81ac5999ff53961ab0fd4ac723c6
natmaxex <nathanrumsey@gmail.com>
2018-04-14 15:07:30 -0400

    Nathan

--- MHJWake/Assets/Nathans Work/script/menu.cs
+++ MHJWake/Assets/Nathans Work/script/menu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour {
 
+    public bool gameTimerIO = false;
+    public float gameTime = 120.0f;
+
 	// Use this for initialization
 	void Start () {
         //PlayerPrefs.SetInt("with or without sound", 20);//with or without sound
@@ -12,17 +15,31 @@ public class menu : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (gameTimerIO == true) {
+            TimeCon();
+        }
+    }
+    void TimeCon() {
+        gameTime -= Time.deltaTime;
+        if (gameTime <= 0.0f) {
+
+        }
+    }
 
     //vocal guide lead, solo guide lead
     public void VocalG() {
+        SceneManager.LoadScene("VocalGuideLeadLevel", LoadSceneMode.Single);
+        Debug.Log("VocalGuideLeadLevel Loading");
+    }
+
+    public void SoloG() {
         SceneManager.LoadScene("SoloGuideLeadLead", LoadSceneMode.Single);
         Debug.Log("SoloGuideLeadLead Loading");
     }
 
-    public void SoloG() {
-        SceneManager.LoadScene("VocalGuideLeadLevel", LoadSceneMode.Single);
-        Debug.Log("VocalGuideLeadLevel Loading");
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Exiting");
     }
 }
